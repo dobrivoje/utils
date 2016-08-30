42b80ae00f3f107d57b9da757a814c3bb35f468d
root <root@192.168.1.103>
2016-08-30 06:52:21 +0200

    30.8.2016 6:51 - Uvođenje apple mac fancy boja, za nove statuse. Koristićemo ove novosti za tabelu logova sa izvršenim komandama.

---
+++ src/main/java/org/superbapps/utils/common/Enums/Statuses2.java
@@ -0,0 +1,35 @@
+package org.superbapps.utils.common.Enums;
+
+/**
+ *
+ * @author д06ри
+ */
+public enum Statuses2 {
+
+    STARTED("started"),
+    ONGOING("ongoing"),
+    EXECUTED("executed"),
+    FAILED("failed"),
+    UNKNOWN("unknown"),
+    //
+    //
+    OK_COLOR("#2dd085"),
+    BAD("#222222"),
+    IN_PROGRESS_COLOR("#F3A344"),
+    UNKNOWN_COLOR("#aa66aa"),
+    //
+    APPLE_RED("#EE5C56"),
+    APPLE_GREEN("#62CB43"),
+    APPLE_YELLOW("#F8BD32");
+
+    private final String name;
+
+    private Statuses2(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
--- src/main/java/org/superbapps/utils/vaadin/FancyLabels/ColorLabels.java
+++ src/main/java/org/superbapps/utils/vaadin/FancyLabels/ColorLabels.java
@@ -5,35 +5,35 @@ import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Label;
 import java.util.HashMap;
 import java.util.Map;
-import org.superbapps.utils.common.Enums.Statuses;
+import org.superbapps.utils.common.Enums.Statuses2;
 
 /**
  *
  * @author д06ри
  */
-public class StatusLabel extends Label {
+public class ColorLabels extends Label {
 
-    private final Map<Statuses, Statuses> color = new HashMap<>();
+    private final Map<Statuses2, Statuses2> color = new HashMap<>();
     private String iconCode;
 
-    public StatusLabel() {
+    public ColorLabels() {
         setContentMode(ContentMode.HTML);
         setSizeUndefined();
 
-        color.put(Statuses.OK, Statuses.OK_COLOR);
-        color.put(Statuses.BLACK_LIST, Statuses.BLACK_LIST_COLOR);
-        color.put(Statuses.IN_PROGRESS, Statuses.IN_PROGRESS_COLOR);
-        color.put(Statuses.UNKNOWN, Statuses.UNKNOWN_COLOR);
-        color.put(Statuses.NO_LICENCE, Statuses.NO_LICENCE_COLOR);
+        color.put(Statuses2.STARTED, Statuses2.APPLE_YELLOW);
+        color.put(Statuses2.IN_PROGRESS_COLOR, Statuses2.APPLE_YELLOW);
+        color.put(Statuses2.FAILED, Statuses2.APPLE_RED);
+        color.put(Statuses2.EXECUTED, Statuses2.APPLE_GREEN);
+        color.put(Statuses2.UNKNOWN, Statuses2.UNKNOWN_COLOR);
     }
 
-    public StatusLabel(Statuses status, String property) {
+    public ColorLabels(Statuses2 status, String property) {
         this();
         render(status);
         setValue(iconCode + " " + property);
     }
 
-    private void render(Statuses status) {
+    private void render(Statuses2 status) {
         iconCode = "<span class=\"v-icon\" style=\"font-family: "
                 + FontAwesome.CIRCLE.getFontFamily()
                 + ";color:"
