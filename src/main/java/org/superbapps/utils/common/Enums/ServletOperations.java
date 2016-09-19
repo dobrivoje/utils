514a3dfad63f3136659ba5ca461a6a2cdf33fc77
root <root@192.168.1.103>
2016-09-19 22:42:40 +0200

    19.9.2016 22:38 - Kozmetika..

--- src/main/java/org/superbapps/utils/common/Enums/ServletOperations.java
+++ src/main/java/org/superbapps/utils/common/Enums/ServletOperations.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.superbapps.utils.common.Enums;
 
 public enum ServletOperations {
 
-    SERVLET_CREATION("SERVLET_SESSION_CREATION___SHIRO_ACCESS_CONTROL"),
+    SERVLET_SHIRO_INIT("SERVLET_SESSION_CREATION___SHIRO_ACCESS_CONTROL_INSTANCE"),
+    SERVLET_SHIRO_AUTHENTICATED("SERVLET_SESSION_CREATION___SHIRO_AUTHENTICATED"),
     SERVLET_DESTRUCTION("SERVLET_SESSION_DESTRUCTION___SHIRO_ACCESS_CONTROL");
 
     private final String name;
