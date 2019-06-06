969367cdd86a36cdbe0bd407eb8a7c0bc5556fda
dobrivoje <dobri7@gmail.com>
2019-06-06 22:54:06 +0200

    pom update utils bug fix

--- src/main/java/org/superbapps/utils/common/Utils.java
+++ src/main/java/org/superbapps/utils/common/Utils.java
@@ -7,7 +7,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author д06ри, dobri7@gmail.com
  */
 public class Utils {
@@ -15,7 +14,8 @@ public class Utils {
     public static final String NL = String.format("%n", "");
 
     public static String capitalize(String str) {
-        return str.substring(0, 1).toUpperCase().concat(str.substring(1));
+        return (str == null || str.length() < 2) ? "" :
+            str.substring(0, 1).toUpperCase().concat(str.substring(1).toLowerCase());
     }
 
     public static String getShorterString(String str) {
@@ -35,31 +35,17 @@ public class Utils {
         return str;
     }
 
-    public static String getLeadingUpperCapital(String str) {
-        if (str == null || str.length() < 2) {
-            return "";
-        } else {
-            return str.substring(0, 1).toUpperCase()
-                    .concat(str.substring(1).toLowerCase());
-        }
-    }
-
     /**
      * Get a string list from a collection. Iterate through a collection, and
      * make a string with "new line/break" termination sign.
      *
-     * @param <T> generic type
+     * @param <T>        generic type
      * @param collection Collection
-     *
      * @return Formatted string in nice form
      */
     public static <T> String getListFromCollection(Collection<T> collection) {
         StringBuilder sb = new StringBuilder();
-
-        collection.stream().forEach((elem) -> {
-            sb.append(elem).append(NL);
-        });
-
+        collection.stream().forEach(elem -> sb.append(elem).append(NL));
         return sb.toString();
     }
 
