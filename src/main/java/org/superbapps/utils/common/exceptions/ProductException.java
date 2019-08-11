4e30e0e6980148408aefb40c528fd296b4e42838
dobrivoje <dobri7@gmail.com>
2019-08-11 13:27:44 +0200

    ProductException

---
+++ src/main/java/org/superbapps/utils/common/exceptions/ProductException.java
@@ -0,0 +1,11 @@
+package org.superbapps.utils.common.exceptions;
+
+/**
+ * @author д06ри, dobri7@gmail.com
+ */
+public class ProductException extends Exception {
+
+    public ProductException(String message) {
+        super(message);
+    }
+}
