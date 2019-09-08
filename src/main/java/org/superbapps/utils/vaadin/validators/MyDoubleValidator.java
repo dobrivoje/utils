05f94f991f9bbbe04f327ec237e2bdbfd9ed1ba1
AUGUSTUS\root <dobri7@gmail.com>
2019-09-08 17:54:27 +0200

    Admin - Mogućnost ručnog upisa cene #62 https://github.com/dobrivoje/rucaj.com/issues/62

--- src/main/java/org/superbapps/utils/vaadin/validators/MyDoubleValidator.java
+++ src/main/java/org/superbapps/utils/vaadin/validators/MyDoubleValidator.java
@@ -5,7 +5,7 @@ import com.vaadin.v7.data.Validator;
 /**
  * @author д06ри
  */
-public class MyNumberValidator implements Validator {
+public class MyDoubleValidator implements Validator {
 
     @Override
     public void validate(Object value) throws InvalidValueException {
@@ -14,7 +14,11 @@ public class MyNumberValidator implements Validator {
         if (text == null || text.trim().isEmpty()) {
             throw new InvalidValueException("Must enter at least zero !");
         }
-        if (!text.matches("\\d*")) {
+
+        // if (!text.matches("^[\\\\+\\\\-]{0,1}[0-9]+[\\\\.\\\\,]{1}[0-9]+$")) {
+        try {
+            Double.parseDouble(text);
+        } catch (NumberFormatException e) {
             throw new InvalidValueException("Just numbers allowed !");
         }
     }
